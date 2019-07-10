38a27773569b106b536fefcb897a8646802d2d78
drjgauthier <40614340+drjgauthier@users.noreply.github.com>
2019-07-10 14:21:50 -0700

    Update app.R
    Added confidence bands of predictions
    Added quadratic function used to simulate 'true' logit values
    Added lines to separate sections

--- app.R
+++ app.R
@@ -6,20 +6,28 @@ library(dplyr)
 library(tidyr)
 theme_set(theme_minimal())
 ui <- fluidPage(
-  titlePanel("Logistic regression with restricted cubic splines to model non-linear effects"),
+  titlePanel("Spliny: restricted cubic splines to model non-linear effects using logistic regression"),
   sidebarLayout(
     sidebarPanel(
       selectInput(inputId = "knots", label = "Select number of knots", choices = c("3","4","5","6","7"),
                   selected = "knots3"),
-      sliderInput("intercept", "Baseline risk", min = -2, max = 2, value = 2),
-      sliderInput("beta","Coefficient 1: tweaks direction, strength and non-linearity of effects", min = -0.01, max = 0.002, value = -0.01),
-      sliderInput("a", "Coefficient 2: tweaks direction, strength and non-linearity of effects", min = -6, max = -5, value = -5),
+      sliderInput("intercept", "Intercept (Baseline risk)", min = -2, max = 2, value = 2),
+      sliderInput("beta","Coefficient 1 (α1): tweaks direction, strength and non-linearity of effects", min = -0.01, max = 0.002, value = -0.01),
+      sliderInput("a", "Coefficient 2 (α2): tweaks direction, strength and non-linearity of effects", min = -6, max = -5, value = -5),
       sliderInput("n", "Number of subjects to simulate:", min = 0, max = 1000, value = 500),
       sliderInput("sd", "Biomarker standard deviation (spread)", min = 10, max = 300, value = 200)
     ),
     mainPanel(plotOutput("ggplot",width = "100%",height = "500px",hover=TRUE),
+              helpText("Shaded areas represent the 95% confidence bands of the predictions"),
+              hr(),
               tableOutput("AIC_t"),
-              h5("NB: a lower AIC (Akaike Information Criterion) value indicates a better fit")
+              textOutput("AIC_TEST"),
+              helpText("NB: a lower AIC (Akaike Information Criterion) value indicates a better fit"),
+              hr(),
+              withMathJax(),
+              helpText("The relationship between the biomarker (x) and the 'true' logit of the event is modeled using the following quadratic function:$$logit(x) = intercept + \\alpha_1 x^2 + 10^{\\alpha_2}x^2$$"),
+              hr(),
+              a(href="https://github.com/drjgauthier/splines/blob/master/app.R","R code")
               )))
 server <- shinyServer(function(input,output){
   biomarker_r <- reactive({
@@ -43,17 +51,29 @@ server <- shinyServer(function(input,output){
   df_r <- reactive({
     data.frame(biomarker=biomarker_r(),event=ytest_r(),trueprobability=prob_r())
   }) 
+  
+  knots_r <<- reactive({
+    switch(input$knots, 
+           "knots3" = c(.10,.5,.90),
+           "knots4" = c(.05,.35,.65,.95),
+           "knots5" = c(.05,.275,.5,.725,.95),
+           "knots6" = c(.05,.23,.41,.59,.77,.95),
+           "knots7" = c(.025,.1833,.3417,.5,.6583,.8167,.975)
+    )
+  })
+  
   output$AIC_t <- renderTable({
     biomarker_r <- biomarker_r()
     df_r <- df_r()
+    knots_r <- knots_r()
     ddist <<- datadist(df_r)
     options(datadist='ddist')
-    knots_s <- switch(input$knots, 
-                       "knots3" = c(.10,.5,.90),
-                       "knots4" = c(.05,.35,.65,.95),
-                       "knots5" = c(.05,.275,.5,.725,.95),
-                       "knots6" = c(.05,.23,.41,.59,.77,.95),
-                       "knots7" = c(.025,.1833,.3417,.5,.6583,.8167,.975)
+    knots_s <<- switch(input$knots, 
+                       "3" = c(.10,.5,.90),
+                       "4" = c(.05,.35,.65,.95),
+                       "5" = c(.05,.275,.5,.725,.95),
+                       "6" = c(.05,.23,.41,.59,.77,.95),
+                       "7" = c(.025,.1833,.3417,.5,.6583,.8167,.975)
     )
     f <- lrm(ytest_r() ~ (biomarker_r))
     f_AIC <- data.frame(round(AIC(f),digits=0))
@@ -62,7 +82,7 @@ server <- shinyServer(function(input,output){
     AIC_t <- cbind(f_AIC,f_rcs_AIC)
     names(AIC_t) <- c("AIC without spline","AIC with spline")
     AIC_t
-  })
+    })
   output$ggplot <- renderPlot({
     biomarker_r <- biomarker_r()
     df_r <- df_r()
@@ -77,25 +97,29 @@ server <- shinyServer(function(input,output){
     )
     #Fit without rcs
     f <- lrm(ytest_r() ~ biomarker_r)
-    pred <- Predict(f,biomarker_r=seq(1,1000,by=1),fun=plogis,conf.int = F,np = 1000)
-    names(pred) <- c("biomarker","predicted_prob")
+    pred <- Predict(f,biomarker_r=seq(1,1000,by=1),fun=plogis,np = 1000)
+    names(pred) <- c("biomarker","predicted_prob","upper","lower")
     #Fit with rcs
     f_rcs <- lrm(ytest_r() ~ rcs(biomarker_r,quantile(biomarker_r,knots_s)))  
-    pred_rcs <- Predict(f_rcs,biomarker_r=seq(1,1000,by=1),fun=plogis,conf.int = F,np = 1000)
-    names(pred_rcs) <- c("biomarker","predicted_prob_s")
+    pred_rcs <- Predict(f_rcs,biomarker_r=seq(1,1000,by=1),fun=plogis,np = 1000)
+    names(pred_rcs) <- c("biomarker","predicted_prob_s","upper_s","lower_s")
     #Data wrangling
     df <- left_join(df_r,pred,by="biomarker")
     df <- left_join(df,pred_rcs,by="biomarker")
-    gather(df,key=prob_type,value=prediction,-biomarker,-event) -> df
-    
-    p <- ggplot(df,aes(x=biomarker,y=prediction)) + 
-      geom_line(data=df,aes(col=prob_type),alpha=0.8,size=1)+
+    gather(df,key=prob_type,value=prediction,-biomarker,-event,-upper,-lower,-upper_s,-lower_s) -> df_g
+    #Graph "true" probabilities and predictions
+    p <- ggplot() + 
+      geom_line(data=df_g,aes(x=biomarker,y=prediction,col=prob_type),alpha=0.8,size=1)+
       xlim(0,1000)+
       labs(x="Biomarker serum concentration",y="Probability of event")+
       scale_color_manual(values=c("red","blue","black"),labels=c("Predicted probabilities without spline","Predicted probabilities with spline","True probabilities"))+
-      geom_point(data=subset(df,prob_type=="trueprobability"),aes(x=biomarker,y=prediction),alpha=0.1,size=4)+
-      theme(axis.title.x = element_text(size=16),axis.title.y = element_text(size=16),axis.text.x = element_text(size=16),axis.text.y = element_text(size=16),legend.title = element_blank(),legend.text = element_text(size=16))
-    p
+      geom_point(data=subset(df_g,prob_type=="trueprobability"),aes(x=biomarker,y=prediction),alpha=0.1,size=4)+
+      theme(axis.title.x = element_text(size=16),axis.title.y = element_text(size=16),axis.text.x = element_text(size=16,margin = margin(t = 0, r = 0, b = 10, l = 0)),axis.text.y = element_text(size=16),legend.title = element_blank(),legend.text = element_text(size=16))
+    p <- p + geom_ribbon(data=df,aes(x=biomarker,ymin=lower,ymax=upper),fill="red",alpha=0.08)
+    p + geom_ribbon(data=df,aes(x=biomarker,ymin=lower_s,ymax=upper_s),fill="blue",alpha=0.08)
+  })
+  output$math <- renderUI({
+    withMathJax(helpText("The relationship between the biomarker (x) and the 'true' logit of the event is modeled using the following quadratic function: logit(x) = intercept + $$\\alpha1$$x + 10^α2*x^2) $$\\alpha1^2$$"))
   })
   })
 shinyApp(ui, server)
